b9de9f2614b9fdb64bce3a21489de48bcf8321ae
Buffoonss <sb6.kuki001@gmail.com>
2019-03-14 22:58:45 +0800

    修改随机数生成机制

--- DiceJrs/RandomGenerator.cpp
+++ DiceJrs/RandomGenerator.cpp
@@ -4,14 +4,10 @@
 
 namespace RandomGenerator
 {
-	inline unsigned long long GetCycleCount()
-	{
-		return static_cast<unsigned long long> (std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
-	}
-
 	int Randint(int lowest, int highest)
 	{
-		std::mt19937 gen(static_cast<unsigned int>(GetCycleCount()));
+    std::random_device GetGene;
+		std::mt19937 gen(static_cast<unsigned int>(GetGene()));
 		std::uniform_int_distribution<int> dis(lowest, highest);
 		return dis(gen);
 	}
