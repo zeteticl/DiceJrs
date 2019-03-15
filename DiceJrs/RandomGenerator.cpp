32b8bcc15d8f5c44c4604eab94c30f28bda422d5
Buffoonss <sb6.kuki001@gmail.com>
2019-03-15 17:43:30 +0800

    fix

--- DiceJrs/RandomGenerator.cpp
+++ DiceJrs/RandomGenerator.cpp
@@ -1,6 +1,16 @@
 #include "RandomGenerator.h"
 #include <random>
 #include <chrono>
+#include <sstream>
+#include <ostream>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include "DiceNetwork.h"
+#include "GlobalVer.h"
+#include "CQEVE_ALL.h"
+#include "EncodingConvert.h"
+#include "Jrs.h"
 
 namespace RandomGenerator
 {
@@ -11,6 +21,17 @@ namespace RandomGenerator
 		std::uniform_int_distribution<int> dis(lowest, highest);
 		return dis(gen);
 	}
+/*
+  int Randint(int lowest, int highest)
+  {
+    ostringstream getRD;
+    getRD << "/integers/?num=1&min=" << lowest << "&max=" << highest << "&col=1&base=10&firmat=plain&rnd=new";
+    string getRDweb = getRD;
+    Network::GET("www.random.org", getRDweb, 443, getRDs);
+    int msgRDs std::stoi(getRDs);
+    return msgRDs;
+  }
+*/
 }
 
 
